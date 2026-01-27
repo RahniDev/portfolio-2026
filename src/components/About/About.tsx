5924a1e82360f244eece4d6a880187d8aae13dd2
RahniDev <rahni32@protonmail.com>
2026-01-27 23:21:31 +0100

    Add media queries

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,15 +6,15 @@ const About = () => {
     <section id="about" className={styles.about}>
       <h2>About Me</h2>
       <div className={styles.aboutContent}>
-       <img
-        src={profileImg}
-        alt="Photo of Rahni"
-        className={styles.profileCircle}
-      />
+        <img
+          src={profileImg}
+          alt="Photo of Rahni"
+          className={styles.profileCircle}
+        />
         <p>
-        I'm a Web Developer with a serious passion for creating full-stack websites which provide a trouble-free UX, sharp design and complex functionality as well as writing clean and efficient code.
-        I love to keep up-to-date with the latest updates on technologies and continuously learn about best practices and new technologies.
-      </p>
+          I'm a Web Developer with a serious passion for creating full-stack websites which provide a trouble-free UX, sharp design and complex functionality as well as writing clean and efficient code.
+          I love to keep up-to-date with the latest updates on technologies and continuously learn about best practices and new technologies.
+        </p>
       </div>
     </section>
   );
