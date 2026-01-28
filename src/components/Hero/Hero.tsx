75fbf57781651f5df3013dc14577e930b73e344c
RahniDev <rahni32@protonmail.com>
2026-01-28 13:45:06 +0100

    Add yellow rectangle on desktop

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,6 @@ const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.yellowBlock}></div>
-    <div className={styles.triangle}></div>
       <div className={styles.heroText}>
       <h1>Rahni De-Meis</h1>
       <h2>Web Developer</h2>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -43,7 +43,6 @@ const Navbar = () => {
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
-        {/* <li><a href="">CV</a></li> */}
       </ul>
     </nav>
   );
