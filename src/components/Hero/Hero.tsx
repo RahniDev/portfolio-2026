d33703b7a799af1e1fe546f92496dc2b62b040ca
RahniDev <rahni32@protonmail.com>
2026-06-06 16:39:01 +0200

    Make CV downloadable

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,12 @@
 import styles from "./hero.module.css";
 import profileImg from '../../assets/rahni.jpg';
+import CV from '../../assets/Rahni-4.pdf'
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.heroLeft}>
-         <img
+        <img
           src={profileImg}
           alt="Photo of Rahni"
           className={styles.profileCircle}
@@ -14,7 +15,7 @@ const Hero = () => {
       <div className={styles.heroText}>
         <h1>Hi, I'm Rahni</h1>
         <h2>Web Developer</h2>
-        <a href="../../assets/Rahni-4.pdf" target="_blank">
+        <a href={CV} download="" target="_blank">
           CV
         </a>
       </div>
